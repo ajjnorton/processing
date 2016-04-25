85aa1e9202ed836225875796bb91f7a7cb8f9704
ajjnorton <anthony@modularity.works>
2016-04-25 14:40:15 +0300

    added text

--- testp5/app.js
+++ testp5/app.js
@@ -12,10 +12,16 @@ function draw(){
 	//background(255);
 	var n = sin(x)*100;
 	var c = cos(y)*100;
+	stroke(0);
 	ellipse(n+(windowWidth/2),c+200,50,50);
 	x=x+0.05+o;
 	y=y+0.05+o;
 	o=o+0.001;
+	stroke(255);
+	fill(0);
+	text(o,100,100);
+	fill(255);
+
 }
 
 function windowResized() {
